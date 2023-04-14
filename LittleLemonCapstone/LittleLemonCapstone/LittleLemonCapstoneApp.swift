6a695b473ade0da5804c288dbf5d4da850fe53b4
Emre Ülkem <98457972+emrlkem@users.noreply.github.com>
2023-04-14 16:38:22 +0300

    Add files via upload

---
+++ LittleLemonCapstone/LittleLemonCapstone/LittleLemonCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonCapstoneApp.swift
+//  LittleLemonCapstone
+//
+//  Created by Emre Ülkem on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
